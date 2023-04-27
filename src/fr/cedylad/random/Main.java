35ce44a1fd328078a35bb848c51f093752be8b07
cedylad <cedricanassi@gmail.com>
2023-04-27 08:47:02 +0200

    first commit

---
+++ src/fr/cedylad/random/Main.java
@@ -0,0 +1,10 @@
+package fr.cedylad.random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author cedri
+ *
+ */
+module Random {
+}
