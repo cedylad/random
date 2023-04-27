af3240528bfc6011bef313c79dbc8e41c19f9ea7
cedylad <cedricanassi@gmail.com>
2023-04-27 08:54:16 +0200

    Generation du random

--- src/fr/cedylad/random/Main.java
+++ src/fr/cedylad/random/Main.java
@@ -1,9 +1,16 @@
 package fr.cedylad.random;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		
+		Random rand = new Random();
+		
+		int n = rand.nextInt(100);
+		
+		System.out.println(n);
 
 	}
 
